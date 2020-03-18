c61581819f8a4d0874651c7f30806e108b84c5b2
Henry <contrerasenrique973@gmail.com>
2020-03-17 21:34:32 -0700

    Inicio de la clase -Triangulo-

---
+++ Triangulo.java
@@ -0,0 +1,11 @@
+/*
+    Clase hija: Triangulo
+    17 - Marzo - 2020
+*/
+
+public class Triangulo extends Poligono {
+    //---ATRIBUTOS---------------------
+    //---CONSTRUCTORES-----------------
+    //---ENCAPSULAMIENTO---------------
+    //---METODOS-----------------------
+}
