550d31526ddaa8c9b6ca30b7396fc6a17e7a7997
Henry <contrerasenrique973@gmail.com>
2020-03-17 21:13:45 -0700

    Fin de la clase -Poligonos-

--- Poligono.java
+++ Poligono.java
@@ -4,5 +4,25 @@
 */
 
 public abstract class Poligono {
+    //---ATRIBUTOS---------------------
+    protected int numeroLados;
+
+    //---CONSTRUCTOR-------------------
+    public Poligono(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+    
+    //---ENCAPSULAMIENTO---------------
+    public int getNumeroLados() {
+        return numeroLados;
+    }
+
+    public void setNumeroLados(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+
+    
+
+    //---METODOS-----------------------
 
 }
