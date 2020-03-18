c5b44cc320bd65dbbeff3022956db6f2cc1222ea
Henry <contrerasenrique973@gmail.com>
2020-03-17 21:54:32 -0700

    Creacion de la clase main -principal-

---
+++ principal.java
@@ -0,0 +1,18 @@
+/*
+    Clase main: principal
+
+    Descripcion:
+    programa para calcular el área de Polígonos (Triángulos y Rectángulos) el programa debe ser capaz de almacenar en un arreglo N triángulos y rectángulos, y al final mostrar el área y los datos de cada uno. Para ello se tendrá lo siguiente: 
+
+    - Una super clase llamada Polígono.
+    - Una sub clase llamada Rectángulo.
+    - Una sub clase llamada Triángulo.
+
+    17 - Marzo - 2020
+*/
+
+public class principal {
+    public static void main(String[] args) {
+        
+    }
+}
